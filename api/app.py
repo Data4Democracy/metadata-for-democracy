1e1c0057e87e9586fad1bb606a2a0f0bc2152d9d
Jake Bladt <jake@ambulnz.com>
2017-05-12 11:21:13 -0400

    +home listeners

--- api/app.py
+++ api/app.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 import os
+from flask import Flask
 
+app = Flask(__name__)
 port = os.getenv('MD4D_API_PORT', '8080')
+
+@app.route("/")
+def home():
+  return 'Hello from the metadata-for-democracy API.'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(port), debug=True)
