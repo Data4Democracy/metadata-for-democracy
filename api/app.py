b4aeec57e36eac35aed23e49755680f32d842309
Jake Bladt <jake@ambulnz.com>
2017-05-12 11:16:26 -0400

    +basic listeners

--- api/app.py
+++ api/app.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+import os
+
+port = os.getenv('MD4D_API_PORT', '8080')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port), debug=True)
--- webhook/app.py
+++ webhook/app.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+import os
+
+port = os.getenv('MD4D_WEBHOOK_PORT', '8079')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(port), debug=True)
